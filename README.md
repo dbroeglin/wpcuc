0b7dec0b873cf9bf08320f27bb00c3e4bbf247e8
Dominique Broeglin <dominique.broeglin@gmail.com>
2020-03-29 20:29:30 +0000

    First skeleton that compiles, cross-compiles and executes on RPi0

--- README.md
+++ README.md
@@ -2,17 +2,20 @@
 
 TBD
 
+## Build setup
+
+    sudo apt install pkg-config
+    sudo apt install libudev-dev
 
 ## Cross compilation to Raspberry PI Zero
 
 ### Setup
 
-Did not work (produces an ARMv7 binary): 
-    rustup target add arm-unknown-linux-gnueabihf
-    sudo apt install g++-arm-linux-gnueabihf
+    rustup target add arm-unknown-linux-musleabihf
+    git clone https://github.com/raspberrypi/tools $HOME/rpi_tools
 
 ### Compilation
 
-    rustup target add arm-unknown-linux-gnueabi
-    git clone https://github.com/raspberrypi/tools $HOME/rpi_tools
-    RUSTFLAGS="-C linker=$HOME/rpi_tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc" cargo build --target arm-unknown-linux-gnueabihf --tests --release
+    PKG_CONFIG_ALLOW_CROSS=1 \
+    RUSTFLAGS="-C linker=$HOME/rpi_tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc" \
+    cargo build --target arm-unknown-linux-musleabihf --tests --release
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    use std::{cell::RefCell, future::Future, io::Error, pin::Pin, rc::Rc, time::Duration};
+
+    use tokio_modbus::client::{
+        rtu,
+        util::{reconnect_shared_context, NewContext, SharedContext},
+        Context,
+    };
+    use tokio_modbus::prelude::*;
+    use tokio_serial::{Serial, SerialPortSettings};
+
+    const SLAVE_1: Slave = Slave(0x01);
+    
+    #[derive(Debug)]
+    struct SerialConfig {
+        path: String,
+        settings: SerialPortSettings,
+    }
+
+    impl NewContext for SerialConfig {
+        fn new_context(&self) -> Pin<Box<dyn Future<Output = Result<Context, Error>>>> {
+            let serial = Serial::from_path(&self.path, &self.settings);
+            Box::pin(async {
+                let port = serial?;
+                rtu::connect(port).await
+            })
+        }
+    }
+
+    let serial_config = SerialConfig {
+        path: "/dev/ttyAMA0".into(),
+        settings: SerialPortSettings {
+            baud_rate: 9600,
+            timeout: Duration::from_millis(100),
+            ..Default::default()
+        },
+    };
+    println!("Configuration: {:?}", serial_config);
+
+    // A shared, reconnectable context is not actually needed in this
+    // simple example. Nevertheless we use it here to demonstrate how
+    // it works.
+    let shared_context = Rc::new(RefCell::new(SharedContext::new(
+        None, // no initial context, i.e. not connected
+        Box::new(serial_config),
+    )));
+
+    reconnect_shared_context(&shared_context).await?;
+
+    assert!(shared_context.borrow().is_connected());
+    println!("Connected");
+
+    println!("Reading a sensor value from {:?}", SLAVE_1);
+    let context = shared_context.borrow().share_context().unwrap();
+    let mut context = context.borrow_mut();
+    context.set_slave(SLAVE_1);
+    let response = context.read_holding_registers(2353, 2).await?;
+    println!("Sensor value for device {:?} is: {:?}", SLAVE_1, response);
+
+
+    Ok(())
 }
