8a545f324ca96ecca2d3944129b9cac20c9f0dbd
Dominique Broeglin <dominique.broeglin@gmail.com>
2020-07-11 20:42:57 +0200

    Start decoding values (multiple read do not work)

--- README.md
+++ README.md
@@ -19,3 +19,25 @@ TBD
     PKG_CONFIG_ALLOW_CROSS=1 \
     RUSTFLAGS="-C linker=$HOME/rpi_tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc" \
     cargo build --target arm-unknown-linux-musleabihf --tests --release
+
+## Build setup for MacOS X
+
+## Cross compilation to Raspberry PI Zero
+
+Source: https://pixelspark.nl/2020/cross-compiling-rust-programs-for-a-raspberry-pi-from-macos
+
+### Setup
+
+    brew install arm-linux-gnueabihf-binutils
+    rustup target add arm-unknown-linux-musleabi
+
+In ~/.cargo/config:
+
+    [target.arm-unknown-linux-musleabi]
+    linker = "arm-linux-gnueabihf-ld"
+    [target.x86_64-linux-musleabi]
+    linker = "x86_64-linux-gnueabihf-ld"
+
+### Compilation
+
+    cargo build --target=arm-unknown-linux-musleabi
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use time::Time;
+use chrono::prelude::*;
+use byteorder::{ByteOrder, LittleEndian};
+use encoding::{Encoding, DecoderTrap};
+use encoding::all::ISO_8859_6;
+
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     use std::{cell::RefCell, future::Future, io::Error, pin::Pin, rc::Rc, time::Duration};
@@ -55,9 +61,112 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let context = shared_context.borrow().share_context().unwrap();
     let mut context = context.borrow_mut();
     context.set_slave(SLAVE_1);
+
+    let response = context.read_holding_registers(2193, 2).await?;
+    println!("OHCompressor1: {:?}", convert_to_float(&response));
+
+    let context = shared_context.borrow().share_context().unwrap();
+    let mut context = context.borrow_mut();
+    context.set_slave(SLAVE_1);
+
+    let response = context.read_holding_registers(2193, 2).await?;
+    println!("OHCompressor1: {:?}", convert_to_float(&response));
+
+    context.set_slave(SLAVE_1);
+
     let response = context.read_holding_registers(2353, 2).await?;
-    println!("Sensor value for device {:?} is: {:?}", SLAVE_1, response);
+    println!("OHDomesticWaterAuxilary: {:?}", convert_to_float(&response));
 
 
     Ok(())
+}
+
+fn convert_to_float(data: &[u16]) -> f32 {
+    let mut buf: [u8; 4] = [0; 4];
+
+    LittleEndian::write_u16(&mut buf, data[0]);
+    LittleEndian::write_u16(&mut buf[2..4], data[1]);
+
+    LittleEndian::read_f32(&buf)
+}
+
+fn convert_to_int(data: &[u16]) -> u16 {
+    data[0]
+}
+
+fn convert_to_byte(data: &[u16]) -> u8 {
+    (data[0] & 0xff) as u8
+}
+
+fn convert_to_time(data: &[u16]) -> Time {
+    Time::try_from_hms(
+        (data[0] >> 8) as u8,
+        (data[0] & 0xff) as u8,
+        0)
+        .expect("coucou")
+}
+
+fn convert_to_datetime(data: &[u16]) -> DateTime<Utc> {
+    // !!! careful, this will panic in case of bad value
+    Utc.ymd(
+        (1900 + data[5]) as i32,
+        data[4] as u32,
+        data[3] as u32)
+        .and_hms(
+            data[2] as u32,
+            data[1] as u32,
+            data[0] as u32)
+}
+
+fn convert_to_string(data: &[u16]) -> String {
+    let mut buf = String::new();
+
+    for i in 0..data.len() {
+        let word = data[i];
+        buf.push_str(&ISO_8859_6.decode(
+            &[(word & 0xff) as u8, (word >> 8) as u8],DecoderTrap::Replace).expect("Unable to decode string")[..]);
+
+    }
+
+    buf
+}
+
+#[test]
+fn to_time() {
+    let t = convert_to_time(&[1056]);
+    assert_eq!(t.hour(), 4);
+    assert_eq!(t.minute(), 32);
+    assert_eq!(t.second(), 0);
+}
+
+#[test]
+fn to_datetime() {
+    let dt = convert_to_datetime(&[1,2,3, 4, 5, 120]);
+    assert_eq!(dt.year(), 2020);
+    assert_eq!(dt.month(), 5);
+    assert_eq!(dt.day(), 4);
+    assert_eq!(dt.hour(), 3);
+    assert_eq!(dt.minute(), 2);
+    assert_eq!(dt.second(), 1);
+}
+
+#[test]
+fn to_floats() {
+    assert_eq!(convert_to_float(&[29226, 17567]), 1275.5676);
+}
+
+
+#[test]
+fn to_int() {
+    assert_eq!(convert_to_int(&[29226]), 29226);
+}
+
+#[test]
+fn to_byte() {
+    assert_eq!(convert_to_byte(&[1057]), 33);
+}
+
+#[test]
+fn to_string() {
+    assert_eq!(convert_to_string(&[0x4349]), "IC");
 }
